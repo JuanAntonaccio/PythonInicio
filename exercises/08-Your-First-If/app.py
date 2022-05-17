60acee9353c115e49affd0f3480bae4aee0ae05a
JuanAntonaccio <jmantonaccio@gmail.com>
2022-05-17 01:05:42 +0000

    cambios python

--- exercises/08-Your-First-If/app.py
+++ exercises/08-Your-First-If/app.py
@@ -1,3 +1,10 @@
 total = int(input('How much money do you have in your pocket\n'))
 
 # YOUR CODE HERE
+
+if total>100:
+    print("dame tu dinero")
+elif total>50:
+    print("Comprame un cafe")
+else:
+    print("eres pobre")        
